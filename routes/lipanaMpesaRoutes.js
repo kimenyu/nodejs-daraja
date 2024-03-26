97960875449deff5bdfd5bf4aaed856c00e626e9
kimenyu <njorogekimenyu@gmail.com>
2024-03-26 10:23:39 +0300

    changing logic

--- routes/lipanaMpesaRoutes.js
+++ routes/lipanaMpesaRoutes.js
@@ -1,16 +1,16 @@
-// import express from 'express'
-// const router = express.Router()
-// import {
-//     initiateSTKPush,
-//     stkPushCallback,
-//     confirmPayment
-// } from "../controllers/lipanampesaController.js"
+import express from 'express'
+const router = express.Router()
+import {
+    initiateSTKPush,
+    stkPushCallback,
+    confirmPayment
+} from "../controllers/lipanampesaController.js"
 
 
-// import {accessToken} from "../middlewares/generateAccessTokenMiddleware.js"
+import {accessToken} from "../middlewares/generateAccessTokenMiddleware.js"
 
-// router.route('/stkPush').post(accessToken,initiateSTKPush)
-// router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
-// router.route('/confirmPayment/:CheckoutRequestID').post(accessToken,confirmPayment)
+router.route('/stkPush').post(accessToken,initiateSTKPush)
+router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
+router.route('/confirmPayment/:CheckoutRequestID').post(accessToken,confirmPayment)
 
-// export default router
+export default router
